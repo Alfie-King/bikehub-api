822d32956479e5c51fa17174917062ebb07b5286
Alfie-King <alfie.king@theinformationlab.co.uk>
2025-11-13 10:57:12 +0000

    fixing load.py and allowing it to loop for all files in the data folder and upload to s3

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+print('packages imported')
+import requests as r
+import time as t
+from datetime import datetime as dt
+import json 
+import os
+import boto3
+import sys
+from dotenv import load_dotenv 
+
 from extract_bikepoint import extract
 from load import load
+print('loading functions')
 
 url = 'https://api.tfl.gov.uk/BikePoint'
 
+print('running extract')
 extract(url)
+print('extract done')
+
+print('running load')
 load()
+print('load done')
+
+print('script finished')
